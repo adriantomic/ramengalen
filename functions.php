9948f8bb9890149b4c175e137dc007bfe40a8957
Adrian Tomic <kontakt@adriantomic.se>
2016-03-22 23:09:09 +0100

    Fixed functions file

--- functions.php
+++ functions.php
@@ -49,3 +49,6 @@ require_once( 'library/responsive-images.php' );
 
 /** If your site requires protocol relative url's for theme assets, uncomment the line below */
 // require_once( 'library/protocol-relative-theme-assets.php' );
+
+/** Ramengalen */
+require_once( 'library/ramengalen/rg-filters.php' );
