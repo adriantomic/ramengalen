80989938d1c7e7735e2a12771674a08223413e88
Adrian Tomic <kontakt@adriantomic.se>
2016-03-30 11:37:01 +0200

    Added lazy load

---
+++ assets/javascript/custom/lazy-load.js
@@ -0,0 +1,12 @@
+/* 
+* @Author: adriantomic
+* @Date:   2016-01-14 16:18:30
+* @Last Modified by:   Adrian Tomic
+* @Last Modified time: 2016-03-30 11:34:48
+*/
+
+(function($) {
+
+ 	$(".entry-content img").lazyload();
+
+})(jQuery);
